f28c38dd5ed11c11cacc3c0f359637587499c7e8
yusuftahaezgin <yusuftahaezgin@gmail.com>
2024-12-09 16:46:08 +0300

    hizmet sınıfı için oluşturma,düzenleme, silme,listeleme işlemlerinin yapılması

--- BerberOtomasyonu/Controllers/AdminController.cs
+++ BerberOtomasyonu/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using BerberOtomasyonu.Models;
 using BerberOtomasyonu.Entity;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace BerberOtomasyonu.Controllers;
 
@@ -13,5 +14,101 @@ public class AdminController : Controller
         {
             _veri=veri;
         }
-        
+
+        [Authorize(Roles = "admin")]
+        public IActionResult Index() 
+        {
+            var hizmetler = _veri.Hizmetler.ToList(); 
+            return View(hizmetler);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        public IActionResult HizmetOlustur()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> HizmetOlustur(Hizmet model){ //musteri ekleme
+            _veri.Hizmetler.Add(model);
+            await _veri.SaveChangesAsync();
+            return RedirectToAction("Index","Admin");
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> HizmetDuzenle(int? id){
+            if(id==null){
+                return NotFound();
+            }
+
+           var hizmet = await _veri.Hizmetler.FindAsync(id); //bu fonk ile sadece id ye gore arama yapabiliriz
+            
+            if(hizmet==null){
+                return NotFound();
+            }
+
+            return View(hizmet);
+        }
+
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> HizmetDuzenle(int id, Hizmet model){ // ogrenci guncelleme
+            if(id!=model.HizmetID){ // route daki id ile modelden gelen id yi karsilastirdik
+                return NotFound();
+            }
+
+           if(ModelState.IsValid){
+            try
+            {
+                _veri.Update(model);
+                await _veri.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if(!_veri.Hizmetler.Any(o=>o.HizmetID == model.HizmetID)){
+                    return NotFound();
+                }
+                else{
+                    throw;
+                }
+            }
+            return RedirectToAction("Index","Admin");
+           }
+           return View(model);
+        }
+
+
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> HizmetSil(int? id){
+            if(id==null){
+                return NotFound();
+            }
+
+           var hizmet = await _veri.Hizmetler.FindAsync(id); 
+
+            if(hizmet==null){
+                return NotFound();
+            }
+
+            return View(hizmet);
+        }
+
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> HizmetSil([FromForm]int id){ //formdaki id ile karsilastirdik
+           var hizmet = await _veri.Hizmetler.FindAsync(id); 
+            if(hizmet==null){
+                return NotFound();
+            }
+            _veri.Hizmetler.Remove(hizmet);
+            await _veri.SaveChangesAsync();
+            return RedirectToAction("Index","Admin");
+        }
 }
--- BerberOtomasyonu/obj/Debug/net8.0/BerberOtomasyonu.AssemblyInfo.cs
+++ BerberOtomasyonu/obj/Debug/net8.0/BerberOtomasyonu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BerberOtomasyonu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1884f4dc0af0be81bdb09f0d3de0f92ddff63b79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ba26bb2ccdaf9cd916383a3a735e033c79fd53e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BerberOtomasyonu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BerberOtomasyonu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
